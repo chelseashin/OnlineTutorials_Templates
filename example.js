422e0b068cc5363e7f24ec7db9bd0fdafc812f8f
chelseashin <chaewonshin95@gmail.com>
2023-03-30 17:44:36 +0900

    230330 | Recently 12 month expression3

--- example.js
+++ example.js
@@ -5,19 +5,23 @@ let currentYear = currentDate.getFullYear(); // get current year
 let monthsList = [];
 
 // map month number to month name abbreviation
-let monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+let monthNames = [
+  'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 
+  'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+];
 
 for (let i = 0; i < 12; i++) {
-    let month = (currentMonth + 12 - i) % 12; // calculate previous month
-    let year = currentYear - Math.floor((currentMonth + 12 - i) / 12); // calculate year for previous month
-    let monthName = monthNames[month]; // get month name from array
-    let yearLastTwo = year.toString().slice(-2); // get last two characters of year as string
-    monthsList.push(`${monthName} ${yearLastTwo}`); // add month and year to list in "MMM YY" format
+  let month = (currentMonth + 12 - i) % 12; // calculate previous month
+  let year = currentYear - Math.floor((currentMonth + 12 - i) / 12); // calculate year for previous month
+  let monthName = monthNames[month]; // get month name from array
+
+  let yearLastTwo = year.toString().slice(-2);
+
+  monthsList.push(`${monthName} ${yearLastTwo}`); // add month and year to list in "MMM YYYY" format
 }
 
-monthsList.reverse(); // reverse the list of months
-
 console.log(monthsList);
+
 
 ////////////////////////////////////////////////////////
 
