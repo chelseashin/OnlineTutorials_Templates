34100bddca3a51b6688734221563511f16e6c24c
chelseashin <chaewonshin95@gmail.com>
2023-03-30 17:11:09 +0900

    230330 | Recently 12 month expression

---
+++ example.js
@@ -0,0 +1,20 @@
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth(); // get current month (0-11)
+let currentYear = currentDate.getFullYear(); // get current year
+
+let monthsList = [];
+
+// map month number to month name abbreviation
+let monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
+for (let i = 0; i < 12; i++) {
+    let month = (currentMonth + 12 - i) % 12; // calculate previous month
+    let year = currentYear - Math.floor((currentMonth + 12 - i) / 12); // calculate year for previous month
+    let monthName = monthNames[month]; // get month name from array
+    let yearLastTwo = year.toString().slice(-2); // get last two characters of year as string
+    monthsList.push(`${monthName} ${yearLastTwo}`); // add month and year to list in "MMM YY" format
+}
+
+monthsList.reverse(); // reverse the list of months
+
+console.log(monthsList);
